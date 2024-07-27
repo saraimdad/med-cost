a6f7dd4c53a40e67b7e7f0b670cd26094ca6927b
saraimdad <saraimdad12@gmail.com>
2024-07-27 21:27:49 +0500

    hyperparameter tuning added

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import numpy as np
 import numpy
 from sqlalchemy import create_engine
 from exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def get_data():
     db_config = {
@@ -38,13 +39,20 @@ def save_obj(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para ,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
